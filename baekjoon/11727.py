9bfc6d41166349a8ca9bdf059d3be31051400941
elevne <cwi5525@naver.com>
2023-08-10 00:46:41 +0900

    백준 풀이 (edited)

--- baekjoon/11727.py
+++ baekjoon/11727.py
@@ -1,14 +1,14 @@
 import sys
 
-
 n = int(sys.stdin.readline())
 
 tile_list = [0] * (n+1)
 
 tile_list[1] = 1
-tile_list[2] = 3
+if n != 1:
+    tile_list[2] = 3
 
 if n > 2:
     for i in range(3, n+1):
         tile_list[i] = tile_list[i-2]*2 + tile_list[i-1]
-print(tile_list[n]%10007)
+print(tile_list[n] % 10007)
