0a0e3885b77b6865561116fa5426d383896e846c
elevne <cwi5525@naver.com>
2023-08-15 15:36:54 +0900

    백준 풀이 (17626 진행 중..)

---
+++ baekjoon/in-process/17626.py
@@ -0,0 +1,19 @@
+# 방법이 몰까...... 고민 필요 ㅜㅜ
+nums = [i**2 for i in range(1, 224)]
+
+n = int(input())
+
+tmp_max_num = 0
+i = -1
+res = 0
+while n > 0:
+    while tmp_max_num <= n:
+        i += 1
+        tmp_max_num = nums[i]
+    n -= nums[i-1]
+    print(i)
+    res += 1
+    i = -1
+    tmp_max_num = 0
+
+print(res)
