3cc1a3da78e8ef11043e9a894232e714b31c64f7
elevne <cwi5525@naver.com>
2023-08-18 09:53:09 +0900

    백준 풀이 (1389 진행 중..)

--- baekjoon/in-process/1389.py
+++ baekjoon/in-process/1389.py
@@ -5,34 +5,20 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-relationships = [[] for _ in range(n + 1)]
-
-distance_map = [[0 for _ in range(n+1)] for _ in range(n+1)]
-
-for _ in range(m):
+distance_map = [[99999] * (n+1)] * (n+1)
+for i in range(m):
     a, b = map(int, input().split())
-    relationships[a].append(b)
-    relationships[b].append(a)
+    distance_map[a][b] = 1
 
-def dfs(start_point, current_point, cnt):
-    next_points = relationships[current_point]
-    for next_point in next_points:
-        d = distance_map[start_point][next_point]
-        if (d == 0 or d > cnt) and next_point != start_point:
-            distance_map[start_point][next_point] = cnt
-            distance_map[next_point][start_point] = cnt
-            dfs(start_point, next_point, cnt+1)
+print(distance_map)  # ...? 진짜 이유 모르겠음... 뭐지..?????
 
 for i in range(1, n+1):
-    dfs(i, i, 1)
-
-kb_nums = [99999]
-
-for i in range(1, n+1):
-    distances = distance_map[i]
-    kb_num = sum(distances)
-    kb_nums.append(kb_num)
-
-min_num = min(kb_nums)
-print(distance_map)
-print(kb_nums.index(min_num))
+    for j in range(1, n+1):
+        for k in range(1, n+1):
+            if i == j or i == k or j == k:
+                continue
+            if distance_map[i][j] == 1:
+                continue
+            d = distance_map[i][k] + distance_map[k][j]
+            if d <= distance_map[i][j]:
+                distance_map[i][j] = d
